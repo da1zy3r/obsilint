1e65b2854f660e17f36e08ad532cfbca9bce378f
da1zy3r <da1zyer@gmail.com>
2026-08-11 23:51:17 +0300

    Add function to fix excessive whitespace

--- vault.py
+++ vault.py
@@ -46,5 +46,37 @@ class Vault:
             self.check_file(file)
         return None
 
+    def fix_file(self, file):
+        if not os.path.isfile(file):
+            return 'The file {} does not exist'.format(file)
+        file_read = open(file, 'r', encoding='utf-8')
+        lines = file_read.readlines()
+        file_read.close()
+        file_write = open(file, 'w', encoding='utf-8')
+        new_lines = []
+        for line in lines:
+            gap_found = False
+            gap_start = 0
+            new_line = ''
+            last_added = 0
+            for ch_idx, ch in enumerate(line):
+                if ch == ' ' and ch_idx > 0 and line[ch_idx - 1] != ' ' and not gap_found:
+                    gap_found = True
+                    gap_start = ch_idx
+                elif ch != ' ' and gap_found:
+                    if ch_idx - gap_start > 1:
+                        new_line += line[last_added:gap_start + 1] + line[ch_idx]
+                        last_added = ch_idx + 1
+                    gap_found = False
+            new_line += line[last_added:]
+            new_lines.append(new_line)
+        file_write.writelines(new_lines)
+        file_write.close()
+        return None
+
     def fix(self):
-        pass
+        for file in self.files:
+            self.fix_file(file)
+            print('The file {} has been fixed'.format(file))
+        print('The files have been fixed successfully')
+        return None

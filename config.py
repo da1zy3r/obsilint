1d83c5adf21eec58ca22239ebdefdfbf1d66f413
da1zy3r <da1zyer@gmail.com>
2026-08-22 23:59:05 +0300

    Fix config writing

--- config.py
+++ config.py
@@ -12,6 +12,9 @@ def write_config(pairs: dict[str, Any]) -> None:
     """Write a key-value pairs to the TOML configuration file."""
     config_file = open('config.toml', 'w')
     for key, value in pairs.items():
-        config_file.write("{} = '{}'\n".format(key, value))
+        if type(value) == str:
+            config_file.write("{} = '{}'\n".format(key, value))
+        else:
+            config_file.write("{} = {}\n".format(key, value))
     config_file.close()
     return None
